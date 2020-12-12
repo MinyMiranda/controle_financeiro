cc9c1102d269fa27052e6027bb4d17d83b9af8b1
ysmine <ysminemiranda@gmail.com>
2020-12-11 23:26:28 -0300

    :construction: Criaçao de rotas :construction:

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+    // Manipulação de usuário
+    Route::group(['prefix' => 'users'], function () {
+        Route::post('/store', [UserController::class, 'store']);
+        Route::get('balance/{cpf}', [UserController::class, 'balance']);
+        Route::get('historic/{cpf}', [UserController::class, 'historic']);
+    });
+
+    // Manipulação da conta
+    Route::group(['prefix' => 'account'], function () {
+        Route::post('/debit', [UserController::class, 'debit']);
+        Route::post('/credit', [UserController::class, 'credit']);
+        Route::post('/transfer', [UserController::class, 'transfer']);
+    });
 });
